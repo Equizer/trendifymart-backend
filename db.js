354909ce4a516939361cb449111530c5a165c5cd
Equizer <130571773+Equizer@users.noreply.github.com>
2024-06-22 18:21:03 +0530

    change the connect string

--- db.js
+++ db.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 
 const connectToMongo = async () => {
   try {
-    await mongoose.connect('mongodb://127.0.0.1:27017/trendifymart', {
+    await mongoose.connect('mongodb+srv://ahadc8:GTIyOqkhtXMStz9k@cluster0.un3fw5h.mongodb.net/trendifymart', {
       useNewUrlParser: true,
       useUnifiedTopology: true
     })
